6d7975ef12b9111fadf6f2a50cc02ed405e7a957
Jlaborne <laborne.j@hotmail.fr>
2024-02-19 12:31:50 +0000

    Task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+// C program to illutrate the use of puts() function
+#include <stdio.h>
+int main()
+{
+    // using puts to print hello world
+ 
+    puts("\"Programming is like building a multilingual puzzle");
+ 
+    return 0;
+}
