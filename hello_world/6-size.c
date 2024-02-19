229267b1e337482a81f0f58e212a057b99f70d05
Jlaborne <laborne.j@hotmail.fr>
2024-02-19 15:06:32 +0000

    Task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,5 +7,15 @@
  */
 int main(void)
 {
-	puts("Size of a char: 1 byte\(s\)");
+	char charType;
+	int intType;
+	long int long_intType;
+	long long int long_long_intType;
+	float floatType;
+
+	printf("Size of char: %zu bytes\n", sizeof(charType));
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of long int: %zu bytes\n", sizeof(long_intType));
+	printf("Size of long long int: %zu bytes\n", sizeof(long_long_intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
 }
