390d2c9504526803c60574c2d785839fd6c260b5
Jlaborne <laborne.j@hotmail.fr>
2024-02-20 12:39:33 +0000

    Task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point for program
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'A' ; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
