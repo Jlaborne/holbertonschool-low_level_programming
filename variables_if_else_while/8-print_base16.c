0752c35c76c164bcba17ea026d48b4b02f85aa92
Jlaborne <laborne.j@hotmail.fr>
2024-02-20 14:54:46 +0000

    task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print all possible combinations of single-digit numbers.
+ * You can only use `putchar` to print to the console.
+ * You can only use `putchar` up to four times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	for (x = 0; x < 10; x++)
+		if (i < 10)
+		{
+			putchar(x + '0');
+		}
+		else
+		{
+			putchar('a' + i - 10);
+		}
+	putchar('\n');
+	return (0);
+}
