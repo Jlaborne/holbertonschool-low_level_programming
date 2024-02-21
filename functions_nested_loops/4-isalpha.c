4defc7ca5c319b7b412c79f7b8ba14863e9f1b94
Jlaborne <laborne.j@hotmail.fr>
2024-02-21 16:09:59 +0000

    task 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,1 +1,15 @@
 #include "main.h"
+
+/**
+ * _isalpha - check if a character is alphabetical
+ *@c: the character to be checked
+ *
+ *Return: 1 if TRUE 0 if FALSE
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
