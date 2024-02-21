0e20d7a48c2f3eef16cf12542d094c82a776dac3
Jlaborne <laborne.j@hotmail.fr>
2024-02-21 20:43:12 +0000

    task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_sign - check if a number is positive or negative
+ *@n: the number to be checked
+ *
+ *Return: 1 if n > 0. 0 if n = 0 and -1 if n < 0
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
