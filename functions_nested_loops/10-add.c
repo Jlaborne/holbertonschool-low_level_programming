88d9fd06817e6b21778a1fd168b8545dc421834a
Jlaborne <laborne.j@hotmail.fr>
2024-02-23 08:30:35 +0000

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ * @x: int
+ * @y: int
+ * Description: Add 2 int and return the summ
+ * Return: the sum of 2 int
+ */
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
