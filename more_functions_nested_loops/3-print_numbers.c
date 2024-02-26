329cae678d169f0fdea101ada48cb9345de9d31a
Jlaborne <laborne.j@hotmail.fr>
2024-02-26 10:16:41 +0000

    Task3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Will print all digit from 0 to 9
+ * Description: prints the numbers, from 0 to 9, followed by a new line.
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int i = 0;
+	for (; i < 10; i++)
+	{
+	_putchar (i);
+	}
+	_putchar ('\n');
+}
