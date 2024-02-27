78dd137eeb01186387be0539851667b784568f0d
Jlaborne <laborne.j@hotmail.fr>
2024-02-27 12:25:29 +0000

    task1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swap the value of a & b
+ * @a : int to swap to b
+ * @a : int to swap to a
+ * Description: swap the value of a to b and b to a
+ * Return: always 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
