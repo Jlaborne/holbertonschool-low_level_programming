cd584e5b3f25feced0a6c76d9f83812a93d8dd84
Jlaborne <laborne.j@hotmail.fr>
2024-02-27 12:12:37 +0000

    task0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int and update it value
+ * @n: Value to update
+ *
+ * Description: 
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
