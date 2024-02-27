88229fde52d7decc0b7107162b3ac9f4efd78aba
Jlaborne <laborne.j@hotmail.fr>
2024-02-27 13:36:33 +0000

    task3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Prints the string
+ * @s: pointer to a string
+ *
+ * Description: Print the string
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+		_putchar(str[length]);
+		length++;
+	}
+}
