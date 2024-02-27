448b9cc5e33ec4b0d48da7eb8ee6423865caf214
Jlaborne <laborne.j@hotmail.fr>
2024-02-27 12:47:08 +0000

    task1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 /**
  * swap_int - swap the value of a & b
  * @a : int to swap to b
- * @a : int to swap to a
+ * @b : int to swap to a
  * Description: swap the value of a to b and b to a
  * Return: always 0
  */
