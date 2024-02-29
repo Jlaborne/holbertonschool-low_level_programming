3f58808feb05f21926929644e3282b5eaa73d9b3
Jlaborne <laborne.j@hotmail.fr>
2024-02-29 14:01:03 +0000

    task0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = j = 0;
+	int i = 0, j = 0;
 
-	while (*s1[i] != '\0')
+	while (dest[i] != '\0')
 		i++;
-	while (*s2[j] != '\0') {
-		*s1[i + j] = *s2[j];
+	while (src[j] != '\0') {
+		dest[i + j] = src[j];
 		j++;
 	}
-	*s1[i + j] = '\0';
+	dest[i + j] = '\0';
+	return (dest);
 }
