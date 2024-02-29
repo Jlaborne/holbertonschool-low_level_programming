77edef248293c0857c9baf912dcba285c6bce2ba
Jlaborne <laborne.j@hotmail.fr>
2024-02-29 13:54:11 +0000

    task0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcat - Function that concatenates string
+ * @dest: appends the string in *src to *des
+ * @src: string to appends
+ * Description: Append a string *dest to *src
+ * Return: The pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = j = 0;
+
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0') {
+		s1[i + j] = s2[j];
+		j++;
+	}
+	s1[i + j] = '\0';
+}
