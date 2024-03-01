a0abdd82390d8f1a90ff866b637538c16a075441
Jlaborne <laborne.j@hotmail.fr>
2024-03-01 10:46:28 +0000

    task5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - Change lowercase letter to uppercase
+ * *: pointer
+ * Description: minus 32 each lowercase letter ascii value
+ * Return: Always 0
+ */
+char *string_toupper(char *s)
+{
+	char *p = s;
+
+	while (*s != '\0')
+	{
+		if (*s >= 'a' && *s <= 'z')
+		{
+			*s = *s - 32;
+		}
+		s++;
+	}
+	return (p);
+}
