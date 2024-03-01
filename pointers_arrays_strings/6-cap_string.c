0ed4d3357c3bb7b6ad0cbc3199df41f0b03da46c
Jlaborne <laborne.j@hotmail.fr>
2024-03-01 13:42:18 +0000

    task6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * cap_string - Capitalize all words of a string
+ * @s: string
+ * Description: Capitalize
+ * Return: Always 0
+ */
+char *cap_string(char *s)
+{
+    char *p = s;
+
+    while (*s != '\0')
+    {
+	if ((*s > 30) && (*s < 35) || (*s > 8) && (*s < 11) || (*s == 44))
+	{
+	    s++;
+	    if (*s >= 'a' && *s <= 'z')
+                *s = *s - 32;
+	}
+	else if (*s == 59 || *s == 46 || *s == 63 || (*s > 39) && (*s < 42))
+	{
+	    s++;
+	    if ((*s >= 'a') && (*s <= 'z'))
+                *s = *s - 32;
+	}
+	else if ((*s == 123) || (*s == 125) || (*s == 46))
+	{
+	    s++;
+	    if (*s >= 'a' && *s <= 'z')
+                *s = *s - 32;
+	}
+    }
+    return (p);
+}
