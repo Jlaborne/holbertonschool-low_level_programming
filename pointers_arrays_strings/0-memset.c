44e0036139195b7df26070b1c2c09abd0bfab159
Jlaborne <laborne.j@hotmail.fr>
2024-03-04 11:02:51 +0000

    task0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - Fils memory with constant byte
+ * @s: Adress
+ * @b: Size of memory
+ * @n: Bytes to fill
+ * Description: Fils memory with constant byte
+ * Return: always
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    unsigned  int i = 0;
+
+    while (i < n)
+    {
+	*(s + i) = b;
+	i++;
+    }
+    return (s);
+}
