b087a6b870e0f1f625cfd0bfdd5c8f15edebb1af
Jlaborne <laborne.j@hotmail.fr>
2024-03-04 15:13:02 +0000

    task4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: The string
+ * @accept: character accepted
+ * Description: the fucntion locates the first occurence of any bytes in accept
+ * Return: the pointer to first string found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+				return (s);
+			a++;
+		}
+		s++;
+	}
+	return (s);
+}
