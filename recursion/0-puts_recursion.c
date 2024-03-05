f871829577053211ba8052a20761cba2d770d3d5
Jlaborne <laborne.j@hotmail.fr>
2024-03-05 14:08:05 +0000

    task0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _put_recursion - Print a string
+ * @s: String to print*
+ * Description: Prints using recursion
+ * Return: always 0
+ */
+void _puts_recursion(char *s)
+{
+	if(*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
