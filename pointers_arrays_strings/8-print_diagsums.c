eb71cbceb23ccb61f70fa3ba68e7ff66aa9c61c4
Jlaborne <laborne.j@hotmail.fr>
2024-03-05 12:25:34 +0000

    task7

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums - prints the sum of diagonals of a square matrix of integers
+ * @s: string to analyze
+ * @accept: characters to match
+ * Description: Analyze a string and return the number of bytes matching
+ * Return: Returns the number of bytes
+ */
+void print_diagsums(int *a, int size)
+{
+	int x, y, sumsa, sumsb;
+
+	sumsa = sumsb = 0;
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			if (x == y)
+				sumsa += *((a + y * size) + x);
+			if (x == size - 1 - y)
+				sumsb += *((a + y * size) + x);
+		}
+	}
+	printf("%d, %d\n", sumsa, sumsb);
+}
