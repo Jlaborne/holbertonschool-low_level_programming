df6d0c089e4143cfe65aa4128636e8cac5522429
Jlaborne <laborne.j@hotmail.fr>
2024-03-05 09:21:00 +0000

    task5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: string to analyze
+ * @needle: substing to locate
+ * Description: Analyze a string and return the number of bytes matching
+ * Return: Returns a pointer of the located substring or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *a = needle;
+		char *b = haystack;
+
+		while (*a != '\0' && *a == *b)
+		{
+			a++;
+			b++;
+		}
+		if (*a == '\0')
+			return(haystack);
+		haystack++;
+	}
+	return(haystack);
+}
