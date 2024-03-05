ec203f3c3d8ffd408df83d248cccb7aa65ea843a
Jlaborne <laborne.j@hotmail.fr>
2024-03-05 10:44:06 +0000

    task

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: chess board to print
+ * Description: print each char of the chess
+ * Return: Always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, y;
+
+	y = 0;
+
+	while (y < 8)
+	{
+		for (i = 0; i < 8; i++)
+		{
+			_putchar(a[y][i]);
+			if (a[y][i] == ' ')
+			{
+				_putchar('\n');
+				break;
+			}
+		}
+		_putchar('\n');
+		y++;
+	}
+}
