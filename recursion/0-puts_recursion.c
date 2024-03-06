c0e116e9525a1f1424b0844ce047744b218e2b93
Jlaborne <laborne.j@hotmail.fr>
2024-03-06 09:52:53 +0000

    task0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,13 @@
 #include "main.h"
-#include <stddef.h>
+
 /**
  * _put_recursion - Print a string
- * @s: String to print*
+ * @s: String to print
  * Description: Prints using recursion
  * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (s == NULL)
-	{
-		_putchar('\n');
-		return;
-	}
 	if (*s == '\0')
 	{
 		_putchar('\n');
