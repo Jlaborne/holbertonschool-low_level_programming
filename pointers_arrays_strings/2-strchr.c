b61cc92b4a19303a06e16cba0006986ed3dd8775
Jlaborne <laborne.j@hotmail.fr>
2024-03-06 08:54:55 +0000

    task2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stddef.h>
+
 /**
  * _strchr - Locates a character in a string
  * @s: The string
@@ -9,11 +9,15 @@
  */
 char *_strchr(char *s, char c)
 {
+	if (s == '\0')
+		return ('\0');
 	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
 		s++;
 	}
+	if (c == '\0')
+		return (s);
 	return ('\0');
 }
