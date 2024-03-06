594f45896f9dfea99a0e62e2e47ae619073e578f
Jlaborne <laborne.j@hotmail.fr>
2024-03-06 15:25:04 +0000

    task1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Check the code
+ * @argc: number of command line arguments
+ * @argv: array of size argc
+ * Description: Program that prints the length of the array
+ * Return: always 0.
+ */
+int main(int argc)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
