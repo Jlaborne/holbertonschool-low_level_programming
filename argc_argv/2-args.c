6ea943aad223a20e71c75e86f9d0108f861df736
Jlaborne <laborne.j@hotmail.fr>
2024-03-07 12:01:21 +0000

    task2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Check the code
+ * @argc: number of command line arguments
+ * @argv: array of size argc
+ * Description: Program that prints all arguments it receives
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	while (argc-- - 1)
+		printf("%s\n", *++argv);
+	return (0);
+}
