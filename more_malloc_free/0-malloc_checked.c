c45a5b35195a98392f2cb434d4b3528f9a752018
Jlaborne <laborne.j@hotmail.fr>
2024-03-12 12:48:33 +0000

    task0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <limits.h>
 #include <stdlib.h>
 /**
  * *malloc_checked - function that allocates memory using malloc
@@ -10,6 +12,6 @@ void *malloc_checked(unsigned int b)
 	void *ptr = malloc(b);
 
 	if (ptr == NULL)
-		exit(EXIT_FAILURE);
+		exit(98);
 	return (ptr);
 }
