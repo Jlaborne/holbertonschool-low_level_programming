064f514a640caa87e9e7c249d69cf0945caf1156
Jlaborne <laborne.j@hotmail.fr>
2024-03-12 10:28:33 +0000

    task2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - Allocates memory for an array
+ * @nmemb: Number of element in the array
+ * @size: Size in bytes of each nmemb
+ * Return: A pointer to the array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
