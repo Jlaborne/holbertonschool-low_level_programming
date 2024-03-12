fffa4d9a053f8fc0b76555bbc915eb7f4651fb3a
Jlaborne <laborne.j@hotmail.fr>
2024-03-12 12:23:56 +0000

    task2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,18 +3,35 @@
 #include <string.h>
 /**
  * str_concat -
- * @s1: the the string
- * @s2:
+ * @s1: First string
+ * @s2: Second string
  * Return: A pointer to the string or NULL if it fails
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *dest;
+	int len1 = 0, len2 = 0, i = 0, j = 0;
 
-	dest = malloc(strlen(s1) + strlen(s2) +1);
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	strcpy (dest, s1);
-	strcat (dest, s2);
+	while (s1 != NULL && s1[len1] != '\0')
+		len1++;
+	while (s2 != NULL && s2[len2] != '\0')
+		len2++;
+	dest = malloc((len1 + len2 + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	while (s1 != NULL && s1[i] != '\0')
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2 != NULL && s2[j] != '\0')
+	{
+
+		dest[i + j] = s2[j];
+		j++;
+	}
+	dest[i + j] = '\0';
 	return (dest);
 }
