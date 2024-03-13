38eb6ec7341f92d522d88e14b8933f473c01337c
Jlaborne <laborne.j@hotmail.fr>
2024-03-13 12:00:59 +0000

    task1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,10 +1,10 @@
 #include "dog.h"
 /**
  * init_dog -  initialize a variable of type struct dog
- * @d:
- * @name:
- * @age:
- * @owner:
+ * @d: Pointer to the struct dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  * Return: Always 0.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
