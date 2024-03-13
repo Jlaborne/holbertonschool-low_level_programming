7e1a513c6d873dcb49eca4f23d03edac4c7e93a5
Jlaborne <laborne.j@hotmail.fr>
2024-03-13 16:39:02 +0000

    task4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -34,7 +34,7 @@ char *_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 		i++;
 	}
-	dest [i] = '\0';
+	dest[i] = '\0';
 		return (dest);
 }
 
@@ -62,7 +62,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog->name == NULL)
 	{
 		free(dog);
-		return(NULL);
+		return (NULL);
 	}
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
