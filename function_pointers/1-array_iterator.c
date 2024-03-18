2950712f9280b19490225fcbc9cf9728a4941fd7
Jlaborne <laborne.j@hotmail.fr>
2024-03-18 10:00:26 +0000

    task1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -9,6 +9,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	if (array == NULL || action == NULL)
+		return;
+	if (size == 0)
+		return;
+	
 	size_t i = 0;
 
 	while (i < size)
