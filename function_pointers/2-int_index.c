b79a21558e92309929d542027f97c4547a87d26a
Jlaborne <laborne.j@hotmail.fr>
2024-03-18 10:19:30 +0000

    task2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * int_index - Iterates through an array and applies a given action to each element
+ * @array: Pointer to the array of integers.
+ * @size: Size of the array.
+ * @cmp: Pointer to the function that compare values
+ * Return: 1 If it's true 2 if it's false
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	if (size == 0)
+		return (-1);
+
+	while (i < size)
+	{
+		if (cmp(array[i]))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
