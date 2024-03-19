cc6b3d010e1d025f1e4d9778578873a4e61fc252
Jlaborne <laborne.j@hotmail.fr>
2024-03-19 11:50:40 +0000

    task 3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stddef.h>
+/**
+ * get_op_func - Retrieves a pointer to a function corresponding to the provided operator
+ * @s: A string representing an arithmetic operator
+ * Return: Pointer to a function that takes two integers and returns an integer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] =
+		{
+			{"+", op_add},
+			{"-", op_sub},
+			{"*", op_mul},
+			{"/", op_div},
+			{"%", op_mod},
+			{NULL, NULL}
+		};
+	int i;
+
+	for (i = 0; ops[i].op != NULL; i++)
+	{
+		if (*ops[i].op == *op)
+		{
+			return (ops[i].f);
+		}
+	}
+}
