838b060cd4a518e33d1e8b5f7e41e48becb204fa
Jlaborne <laborne.j@hotmail.fr>
2024-03-19 13:02:26 +0000

    task3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stddef.h>
+#include <stdio.h>
 /**
  * get_op_func - Retrieves a pointer to a function corresponding to the provided operator
  * @s: A string representing an arithmetic operator
@@ -20,9 +21,11 @@ int (*get_op_func(char *s))(int, int)
 
 	for (i = 0; ops[i].op != NULL; i++)
 	{
-		if (*ops[i].op == *op)
+		if (*ops[i].op == *s)
 		{
 			return (ops[i].f);
 		}
 	}
+	return (ops[i].f);
+	printf("Error\n");
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,3 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
 int main(int argc, char* argv[])
 {
-	
+	int num1, num2;
+	char *operator;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	operator = argv[3];
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	if (get_op_func(operator) == NULL)
+		return (99);
+	if ((*operator == '/' && num2 == 0) || (*operator == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		return(100);
+	}
+
+	printf ("%d\n",get_op_func(operator)(num1, num2));
+	return (0);
+}
