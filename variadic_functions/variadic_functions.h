64fe1329a86a3eb932f51e7d8bf7b2179c8a42b9
Jlaborne <laborne.j@hotmail.fr>
2024-03-20 15:03:14 +0000

    task3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -6,4 +6,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct type_arg
+{
+    char *first_char;
+    char *(*f)();
+} op_t;
+
 #endif
