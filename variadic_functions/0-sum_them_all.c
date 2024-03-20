85374cf7112c02b4507286ea2161b935685c16b3
Jlaborne <laborne.j@hotmail.fr>
2024-03-20 12:15:45 +0000

    task0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,7 +11,10 @@ int sum_them_all(const unsigned int n, ...)
 	va_list list;
 	unsigned int i, sum;
 
-	i= sum = 0;
+	if (n == 0)
+		return (0);
+	
+	i = sum = 0;
 
 	va_start(list, n);
 	while (i < n)
