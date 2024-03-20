fefbb6e4016f075ec12167038b04213971523265
Jlaborne <laborne.j@hotmail.fr>
2024-03-20 13:33:58 +0000

    task2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -4,20 +4,29 @@
  * print_strings - prints strings followed by a new line
  * @separator: the string to be printed between strings
  * @n: the number of strings to print
- * Description: This function takes a variable number of strings and prints
- *              them to the standard output, separated by the specified separator.
+ * Description: This function takes a variable number of strings and prints them
+ *              to the standard output, separated by the specified separator.
  *              It then prints a new line at the end.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
 	unsigned int i;
+	char *str;
 
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s",va_arg(list, char *));
+		str = va_arg(list, char *);
+		if (str != NULL)
+		{
+			printf("%s", str);
+		}
+		else
+		{
+			printf("(nil)");
+		}
 		if (i < n -1 && separator != NULL)
 			printf("%s", separator);
 	}
