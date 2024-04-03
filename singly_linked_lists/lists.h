94bed7466e90099dd566c9ba806f914cf7489783
Jlaborne <laborne.j@hotmail.fr>
2024-04-03 09:25:02 +0000

    Task0

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LINKED_H
+#define LINKED_H
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * struct linked_list - Typedef struct
+ * @char:
+ * @len: Format
+ * @linked_list: The function associated
+ **/
+typedef struct linked_list
+{
+	char *str;
+	unsigned int len;
+	struct linked_list *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
