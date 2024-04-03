1d1be023ed4d8fe8af5c055f1c462b9bd8fb1c19
Jlaborne <laborne.j@hotmail.fr>
2024-04-03 09:31:30 +0000

    Add README

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,11 +6,13 @@
  *
  * Return: The number of nodes in h.
  */
-size_t print_list(const list_t *h) {
+size_t print_list(const list_t *h)
+{
 	size_t count = 0;
 	const list_t *current = h;
 
-	while (current != NULL) {
+	while (current != NULL)
+	{
 		if (current->str == NULL)
 			printf("[0] (nil)\n");
 
@@ -18,7 +20,7 @@ size_t print_list(const list_t *h) {
 			printf("%s\n", current->str);
 		count++;
 		current = current->next;
-    }
+	}
 
 	return (count);
 }
---
+++ singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+PROJECT: C - Singly linked lists
