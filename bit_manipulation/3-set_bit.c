7de03a60f423343c97ed37bf6fa9fa5f2e8f4135
Jlaborne <laborne.j@hotmail.fr>
2024-04-05 08:57:31 +0000

    task3

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -9,12 +9,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned int) * 8)
+	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
-	unsigned int mask = 1 << index;
 
-	*n |= mask;
+	*n ^= (1 << index);
 	return (1);
 }
