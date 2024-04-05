3a12a32171f0197130c5b8cca3b0f52ddcb84dfa
Jlaborne <laborne.j@hotmail.fr>
2024-04-05 10:00:05 +0000

    Added description

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
+ * get_bit - Returns the value of a bit at a given index
+ * @n: The nubmer
+ * @index: the given index
+ * Return: The value of the bit
  *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
